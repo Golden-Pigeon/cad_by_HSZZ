857942b723ccebfd339799cbefb5394e271755b3
Golden-Pigeon <goldenpigeon2000@outlook.com>
2020-06-21 22:49:09 +0800

    demo

--- src/main/java/cad/Main.java
+++ src/main/java/cad/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1051.0, 692.0));
         primaryStage.show();
     }
 
---
+++ src/main/java/cad/Status.java
@@ -0,0 +1,7 @@
+package main.java.cad;
+
+public class Status {
+    public static boolean isLineMode = false;
+    public static double startX = -1.0;
+    public static double startY = -1.0;
+}
