b302aedc62e96c5f5ae447c0722116f735748f97
Golden-Pigeon <goldenpigeon2000@outlook.com>
2020-06-23 23:53:11 +0800

    update two tool bar and some actions

--- src/main/java/cad/Controller.java
+++ src/main/java/cad/Controller.java
@@ -1,15 +1,32 @@
 package main.java.cad;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+
+import javax.swing.*;
 
 public class Controller {
 
+
+    @FXML
+    private CheckMenuItem painterToolBarMenuItem;
+
+    @FXML
+    private CheckMenuItem colorToolBarMenuItem;
+
     @FXML
     private Button mouse;
+
+
+
 
 
     @FXML
@@ -17,6 +34,12 @@ public class Controller {
 
     @FXML
     private Button line;
+
+    @FXML
+    private VBox colorToolBar;
+
+    @FXML
+    private VBox painterToolBar;
 
     @FXML
     private AnchorPane mainPane;
@@ -51,7 +74,27 @@ public class Controller {
 
     }
 
+    @FXML
+    public void onPainterToolMenuItemAction(ActionEvent event){
+        if(painterToolBar.isVisible()) {
+            painterToolBar.setVisible(false);
+            painterToolBarMenuItem.setSelected(false);
+        }
+        else {
+            painterToolBar.setVisible(true);
+            painterToolBarMenuItem.setSelected(true);
+        }
+    }
 
-
-
+    @FXML
+    public void onColorToolMenuItemAction(ActionEvent actionEvent) {
+        if(colorToolBar.isVisible()) {
+            colorToolBar.setVisible(false);
+            colorToolBarMenuItem.setSelected(false);
+        }
+        else {
+            colorToolBar.setVisible(true);
+            colorToolBarMenuItem.setSelected(true);
+        }
+    }
 }
--- src/main/java/cad/Main.java
+++ src/main/java/cad/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
