82cca5112d0c4fa18f9726c025d88298db7343f1
HorizonChaser <ahorizonchaser@email.com>
2020-06-24 23:14:26 +0800

    对工具栏添加了图标, 颜色栏添加了颜色显示 添加了ColorPicker, 修改了标签字号 右下角加入为显示状态预留的Label

--- src/main/java/cad/Main.java
+++ src/main/java/cad/Main.java
@@ -4,18 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import main.java.cad.CommonDefinitions.CommonPath;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("JavaFX CAD Utility");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image(CommonPath.LOGO));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
