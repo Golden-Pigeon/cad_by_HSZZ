3c47fa3b0bb5de7a21fff91ca102df21f3250252
HorizonChaser <ahorizonchaser@email.com>
2020-06-25 12:45:17 +0800

    工具栏添加了Slider与ComboBox, 来选择粗细/字号大小 并实现了这两个控件的互操作(改变一个, 另一个跟着变)

--- src/main/java/cad/Controller.java
+++ src/main/java/cad/Controller.java
@@ -1,17 +1,16 @@
 package main.java.cad;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
 
-import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -32,6 +31,14 @@ public class Controller {
     @FXML
     private AnchorPane mainPane;
 
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private ComboBox<String> sizeComboBox;
+
+    @FXML
+    private Slider sizeSlider;
 
     @FXML
     public void onPanePressed(MouseEvent event) {
@@ -39,12 +46,11 @@ public class Controller {
     }
 
     @FXML
-    public void onPainterToolMenuItemAction(ActionEvent event){
-        if(painterToolBar.isVisible()) {
+    public void onPainterToolMenuItemAction(ActionEvent event) {
+        if (painterToolBar.isVisible()) {
             painterToolBar.setVisible(false);
             painterToolBarMenuItem.setSelected(false);
-        }
-        else {
+        } else {
             painterToolBar.setVisible(true);
             painterToolBarMenuItem.setSelected(true);
         }
@@ -52,13 +58,56 @@ public class Controller {
 
     @FXML
     public void onColorToolMenuItemAction(ActionEvent actionEvent) {
-        if(colorToolBar.isVisible()) {
+        if (colorToolBar.isVisible()) {
             colorToolBar.setVisible(false);
             colorToolBarMenuItem.setSelected(false);
-        }
-        else {
+        } else {
             colorToolBar.setVisible(true);
             colorToolBarMenuItem.setSelected(true);
         }
+    }
+
+    @FXML
+    public void onColorButtonClicked(ActionEvent actionEvent) {
+        Button name = (Button) actionEvent.getSource();
+        System.out.println(name.getId());
+    }
+
+    @FXML
+    public void onColorPickerFinished(ActionEvent actionEvent) {
+        CadCurrentStat.color = colorPicker.getValue();
+        System.out.println(CadCurrentStat.color.toString());
+    }
+
+    @FXML
+    public void onSizeComboBoxClicked(MouseEvent mouseEvent) {
+        List<String> sizeOptions = new ArrayList<>();
+        for (int i = 8; i <= 20; i += 2) {
+            sizeOptions.add(String.valueOf(i));
+        }
+
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        sizeOptions
+                );
+        sizeComboBox.setItems(options);
+        sizeComboBox.setEditable(false);
+    }
+
+    @FXML
+    public void onSizeComboBoxFinished(ActionEvent actionEvent) {
+        String currComboBoxValue = sizeComboBox.getValue();
+        if (currComboBoxValue != null) {
+            CadCurrentStat.textFontSize = (int) Double.parseDouble(currComboBoxValue);
+            CadCurrentStat.lineWidth = (int) Double.parseDouble(currComboBoxValue);
+            sizeSlider.setValue((int) Double.parseDouble(currComboBoxValue));
+        }
+    }
+
+    @FXML
+    public void onSizeSliderFinished(MouseEvent mouseEvent) {
+        sizeComboBox.setValue(String.valueOf(sizeSlider.getValue()));
+        CadCurrentStat.textFontSize = (int) sizeSlider.getValue();
+        CadCurrentStat.lineWidth = (int) sizeSlider.getValue();
     }
 }
